689a8339215ca4c591d590939886140e6baf050c
giannoudak <giannoudak@gmail.com>
2014-02-17 00:06:03 +0200

    new method for api/sights/{id}

--- CULTMACEDONIA_v2/Controllers/MobilePointsDetailController.cs
+++ CULTMACEDONIA_v2/Controllers/MobilePointsDetailController.cs
@@ -3,14 +3,15 @@ using CULTMACEDONIA_v2.Models.CultMacedoniaModel;
 using CULTMACEDONIA_v2.Models.DTOs;
 using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
-using System.Web.Helpers;
 using System.Web.Http;
 using System.Web.Http.Description;
-
 
 
 namespace CULTMACEDONIA_v2.Controllers
@@ -65,60 +66,62 @@ namespace CULTMACEDONIA_v2.Controllers
             return sights;
 
         }
-        /*
+        
         [Route("{id:int}")]
         [ResponseType(typeof(PointDetailDto))]
         public async Task<IHttpActionResult> GetSights(int id)
         {
 
             
-            //var sight = await (from p in db.Point.Include("PointImage").Include("PointVideo").Include("AspNetUsers")
-            //                        join u in db.PointOfUser on p.PointId equals u.PointId
-            //                        where p.PointId == id
-            //                        select new PointDetailDto
-            //                        {
-            //                            PointId = p.PointId,
+            var sight = await (from p in db.Point.Include("PointImage").Include("PointVideo").Include("AspNetUsers")
+                                    join u in db.PointOfUser on p.PointId equals u.PointId
+                                    where p.PointId == id
+                                    select new PointDetailDto
+                                    {
+                                        PointId = p.PointId,
 
-            //                            PointName = p.PointName,
-            //                            PointX = p.PointX,
-            //                            PointY = p.PointY,
+                                        PointName = p.PointName,
+                                        PointX = p.PointX,
+                                        PointY = p.PointY,
 
-            //                            PointYear = p.PointYear,
+                                        PointYear = (p.PointYear == null) ? 0 : p.PointYear,
 
-            //                            PointPlaceNomos = (p.PointPlaceNomos == null) ? string.Empty : p.PointPlaceNomos,
-            //                            PointDescription = p.PointDescription,
-            //                            PointFoto = (p.PointImage.FirstOrDefault().ImageFilePath == null) ? "http://www.cult-macedonia.com/myData/img/default_picture.png" : p.PointImage.FirstOrDefault().ImageFilePath,
+                                        PointPlaceNomos = (p.PointPlaceNomos == null) ? "-" : p.PointPlaceNomos,
+                                        PointDescription = p.PointDescription,
+                                        PointFoto = (p.PointImage.FirstOrDefault().ImageFilePath == null) ? "http://www.cult-macedonia.com/myData/img/default_picture.png" : p.PointImage.FirstOrDefault().ImageFilePath,
+                                        
+                                        PointCategoryId = p.Category.CategoryName,
+                                        PointEraId = p.Era.EraName,
+                                        PointEthnologicalId = p.Ethnological.EthnologicalName,
+                                        PointPropertyId = p.Property.PropertyName,
+                                        PointProtectionId = p.ProtectionLevel.ProtectionName,
+                                        PointReligionId = p.Religion.ReligionName,
 
-            //                            PointCategoryId = p.Category.CategoryName,
-            //                            PointEraId = p.Era.EraName,
-            //                            PointEthnologicalId = p.Ethnological.EthnologicalName,
-            //                            PointPropertyId = p.Property.PropertyName,
-            //                            PointProtectionId = p.ProtectionLevel.ProtectionName,
-            //                            PointReligionId = p.Religion.ReligionName,
+                                        PointVideo = (p.PointVideo.FirstOrDefault().VideoFilePath == null) ? "-" : p.PointVideo.FirstOrDefault().VideoFilePath,
+                                        PointAddress = (p.PointAddress == null) ? "-" : p.PointAddress,
+                                        PointWeb = (p.PointWeb == null) ? "-" : p.PointWeb,
+                                        PointEmail = (p.PointEmail == null) ? "-" : p.PointEmail,
+                                        PointPhone = (p.PointPhone == null) ? "-" : p.PointPhone,
+                                        PointUser = u.AspNetUsers.UserName,
+                                        PointLocalization = p.PointLocalization
 
-            //                            PointVideo = (p.PointVideo.FirstOrDefault().VideoFilePath == null) ? string.Empty : p.PointVideo.FirstOrDefault().VideoFilePath,
-            //                            PointAddress = (p.PointAddress == null) ? string.Empty : p.PointAddress,
-            //                            PointWeb = (p.PointWeb == null) ? string.Empty : p.PointWeb,
-            //                            PointUser = u.AspNetUsers.UserName,
-            //                            PointLocalization = p.PointLocalization
-
-            //                        });
+                                    }).FirstOrDefaultAsync();
             
-            //var f = fro
+          
             
-            //if (sight == null)
-            //{
-            //    return NotFound();
-            //}
+            if (sight == null)
+            {
+                return NotFound();
+            }
 
-            //return Ok(sight);
+            return Ok(sight);
 
             
 
 
           
         }
-        */
+        
 
 
         protected override void Dispose(bool disposing)
